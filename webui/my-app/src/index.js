8aae65295cd6fff6c190c42c04b60856317ba6c4
root <root@Qi_Li_Asus.localdomain>
2022-02-01 18:16:15 +0100

    Buttons and reusable Taskbar

--- webui/my-app/src/index.js
+++ webui/my-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Frame3 from './Frames/Frame3'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
