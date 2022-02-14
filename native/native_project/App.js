16a40b2fa511bd0be7d68993fbd0170b858cb182
root <root@Qi_Li_Asus.localdomain>
2022-02-14 22:17:56 +0100

    backend development ready

---
+++ native/native_project/App.js
@@ -0,0 +1,111 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+//Pages
+import OCR_Page from './src/components/OCR_Page';
+import DogPage from './src/components/DogPage';
+import Main from './src/components/Main';
+import NewItem from './src/components/NewItem';
+import Statistics from './src/components/Statistics';
+import BottomBar from './src/components/BottomBar';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="OCR_Page" component={OCR_Page} />
+        <Stack.Screen name="DogPage" component={DogPage} />
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="NewItem" component={NewItem} />
+        <Stack.Screen name="Statistics" component={Statistics} />
+        <Stack.Screen name="BottomBar" component={BottomBar} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
